1fdea5f8f3e2964da5133071704ad744de47f95f
Alexei Guevara <alexguev@gmail.com>
2015-02-22 23:22:14 -0500

    ex3 cost function

--- mlclass-ex3-008/mlclass-ex3/lrCostFunction.m
+++ mlclass-ex3-008/mlclass-ex3/lrCostFunction.m
@@ -37,6 +37,14 @@ grad = zeros(size(theta));
 %
 
 
+h = sigmoid(X * theta);
+
+J = (1/m) * sum(-y .* log(h) - (ones(m,1) - y) .* log(ones(m,1) - h)) + (lambda / (2*m)) * sum(theta(2:end) .^ 2);
+
+grad = zeros(size(theta));
+
+grad(1,:) = (X'(1,:) * (h - y)) .* (1/m);
+grad(2:end,:) = (X'(2:end,:) * (h - y)) .* (1/m) + ((lambda / m) * theta(2:end));
 
 
 
