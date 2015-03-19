814ec3c36e7e15b3d8dcca0a8b0e4f228eb7ca59
Alexei Guevara <alexguev@gmail.com>
2015-03-18 22:50:16 -0400

    ex6 - problem 1,2

--- mlclass-ex6-008/mlclass-ex6/dataset3Params.m
+++ mlclass-ex6-008/mlclass-ex6/dataset3Params.m
@@ -9,7 +9,7 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 
 % You need to return the following variables correctly.
 C = 1;
-sigma = 0.3;
+sigma = 0.1;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return the optimal C and sigma
@@ -23,11 +23,27 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+C_vec = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+sigma_vec = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
 
+lowest_e = 10000000;
 
+for _C = C_vec
+  for _sigma = sigma_vec
+    fprintf("\n Trying with C = %f, sigma = %f \n", _C, _sigma);
+    model = svmTrain(X, y, _C, @(x1, x2) gaussianKernel(x1, x2, _sigma));
+    predictions = svmPredict(model, Xval);
+    e = mean(double(predictions ~= yval))
+    if (e < lowest_e)
+      best_C = _C
+      best_sigma = _sigma
+      lowest_e = e
+    endif
+  end
+end
 
-
-
+C = best_C
+sigma = best_sigma
 
 % =========================================================================
 
